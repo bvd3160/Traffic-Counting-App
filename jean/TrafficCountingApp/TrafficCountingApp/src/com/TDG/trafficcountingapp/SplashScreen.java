d912f8f6c9b8a05189c7ee2fed45f30c18461bed
Richard <Richard@Richard-PC>
2014-11-26 14:50:04 +1300

    Jean-Yves work

---
+++ jean/TrafficCountingApp/TrafficCountingApp/src/com/TDG/trafficcountingapp/SplashScreen.java
@@ -0,0 +1,59 @@
+package com.TDG.trafficcountingapp;
+
+import java.security.KeyStore.LoadStoreParameter;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends ActionBarActivity {
+	
+	private ProgressBar loadSpinner;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+		loadSpinner = (ProgressBar) findViewById(R.id.loadSpinner);
+		loadSpinner.setVisibility(View.VISIBLE);
+		
+		//Thread to time how long the loading screen is displayed
+		Thread loadTimer = new Thread(){
+			@Override
+			public void run(){
+				try {
+					sleep(6000);
+					startActivity(new Intent(getApplicationContext(), MainScreen.class));
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			}
+		};
+		loadTimer.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.splash_screen, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
