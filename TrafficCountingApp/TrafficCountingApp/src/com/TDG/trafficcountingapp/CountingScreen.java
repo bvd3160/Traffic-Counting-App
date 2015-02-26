466f38d8abcec185dcd40bdc32247cabe9465ebb
Richard Fong <khaos_dash@msn.com>
2015-02-26 13:29:42 +1300

    -Implemented a directional count. However, I need to refine it more because it currently does not update the current count object when I click on a new count. Instead it only updates the current count object when I increase the count of that object.

--- TrafficCountingApp/TrafficCountingApp/src/com/TDG/trafficcountingapp/CountingScreen.java
+++ TrafficCountingApp/TrafficCountingApp/src/com/TDG/trafficcountingapp/CountingScreen.java
@@ -18,7 +18,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class CountingScreen extends ActionBarActivity implements Communicator {
+public class CountingScreen extends ActionBarActivity implements Communicator, OnClickListener{
 
 	/*
 	 * An 'Count Object' in this project will be used as the term for the following: (Unless we can find a better name for them) 
@@ -40,11 +40,15 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 
 	TextView txt_totalCount;
 	int totalCount;
+	String directionFrom;
+	String directionTo;
 	
 	TextView txt_currentObject;
 	TextView txt_timer;
+	TextView txt_currentObjectCount;
 	
 	String comments;
+	String currentlySelectedObject;
 	String intersectionType;
 	
 	Button btn_direction_nw, btn_direction_n, btn_direction_ne, btn_direction_w,
@@ -65,22 +69,55 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 			intersectionType = "No Intersection";
 		}
 		
+		// Initialises the current counting object to be a car
+		updateCurrentObjectTo("Car");
+				
+		initialiseCountObjects();
+		initialiseDirectionFromTo();
 		populateTimer();
 		populateButtons();
 		showCountingPanelAndButtons();
-		
-		// Initialise the count as 0
-		totalCount = 0;
-
-		// Find the counter and set the text with the right count
-		txt_totalCount = (TextView) findViewById(R.id.cs_txt_totalCounter);
-		txt_totalCount.setText("Total Count: " + totalCount);
-
-		// Initialises the current counting object to be a car
-		txt_currentObject = (TextView) findViewById(R.id.cs_txt_currentlyselectedobject);
-		txt_currentObject.setText("Car");
 	}
 	
+	private void initialiseCountObjects(){
+		totalCount = 0;
+		txt_totalCount = (TextView) findViewById(R.id.cs_txt_totalCounter);
+		txt_totalCount.setText("Total Count: " + totalCount);
+		
+		bus = 0;
+		truck = 0;
+		car = 0;
+		motorBike = 0;
+		pedestrian = 0;
+		bike = 0;
+		cane = 0;
+		dog = 0;
+		mobilityScooter = 0;
+		artificialLimb = 0;
+		crutches = 0;
+		walkingFrame = 0;
+		backBrace_visible = 0;
+		backBrace_notVisible = 0;
+		legBrace_visible = 0;
+		legBrace_notVisible = 0;
+		wheelChair_assisted = 0;
+		wheelChair_manual = 0;
+		wheelChair_powered = 0;
+		
+		updateCurrentlySelectedObject(car);
+	}
+	
+	private void initialiseDirectionFromTo(){
+		directionTo = null;
+		directionFrom = null;
+	}
+	
+	private void updateCurrentObjectTo(String object){
+		txt_currentObject = (TextView) findViewById(R.id.cs_txt_currentlyselectedobject);
+		txt_currentObject.setText(object);
+		
+		currentlySelectedObject = object;
+	}
 	
 	/*
 	 * This method will determine what type of setup will be displayed in on the count panel
@@ -117,19 +154,6 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 			setVisibilityDirectionButtons(setVisibleButtons(0));
 			break;
 		}
-		
-//		String intersectionType = CountSetup.getTypeOfIntersection();
-//		if(intersectionType == "3 Way Intersection"){
-//			countPanel.setImageResource(drawable.intersection_3);
-//		}else if(intersectionType == "4 Way Intersection"){
-//			countPanel.setImageResource(drawable.intersection_4);
-//		}else if(intersectionType == "5 Way Intersection"){
-//			countPanel.setImageResource(drawable.intersection_5);
-//		}else if(intersectionType == "6 Way Intersection"){
-//			countPanel.setImageResource(drawable.intersection_6);
-//		}else{
-//			countPanel.setVisibility(8);
-//		}
 	}
 	
 	/*
@@ -217,8 +241,14 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 	 * This will update all the counters. 
 	 * This includes the total count and the count of each count object
 	 */
-	private void updateCounter() {
+	private void updateCounters() {
 		txt_totalCount.setText("Total Count: " + totalCount);
+		
+	}
+	
+	private void updateCurrentlySelectedObject(int currentObject){
+		txt_currentObjectCount = (TextView) findViewById(R.id.cs_txt_currently_selected_count);
+		txt_currentObjectCount.setText(currentlySelectedObject + ": " + currentObject);
 	}
 
 	/*
@@ -234,17 +264,26 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 	 */
 	private void populateDirectionButtons(){
 		btn_direction_nw = (Button)findViewById(R.id.cs_btn_direction_nw);
+		btn_direction_nw.setOnClickListener(this);
 		btn_direction_n = (Button)findViewById(R.id.cs_btn_direction_n);
+		btn_direction_n.setOnClickListener(this);
 		btn_direction_ne = (Button)findViewById(R.id.cs_btn_direction_ne);
+		btn_direction_ne.setOnClickListener(this);
 		btn_direction_w = (Button)findViewById(R.id.cs_btn_direction_w);
+		btn_direction_w.setOnClickListener(this);
 		btn_direction_e = (Button)findViewById(R.id.cs_btn_direction_e);
+		btn_direction_e.setOnClickListener(this);
 		btn_direction_sw = (Button)findViewById(R.id.cs_btn_direction_sw);
+		btn_direction_sw.setOnClickListener(this);
 		btn_direction_s = (Button)findViewById(R.id.cs_btn_direction_s);
+		btn_direction_s.setOnClickListener(this);
 		btn_direction_se = (Button)findViewById(R.id.cs_btn_direction_se);
+		btn_direction_se.setOnClickListener(this);
 	}
 	
 	private void populateTimer(){
 		// Instantiates the CountDownTimer.
+		
 		/*
 		 * We will count down in 0.5 seconds instead of 1 second because long is a
 		 * "Rough" estimate so therefore we may skip some seconds. 
@@ -252,6 +291,7 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 		 * By updating every 0.5 seconds will result in a more accurate measurement.
 		 * Also, start with the time + 500 milliseconds so we can display the initial number
 		 */
+		
 //15m	CountDownTimer countTimer = new CountDownTimer(900500, 500);
 		final CountDownTimer countTimer = new CountDownTimer(5500, 500);
 		
@@ -261,7 +301,6 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 					
 					@Override
 					public void onClick(View v) {
-						// TODO Auto-generated method stub
 						countTimer.start();
 					}
 				});
@@ -270,7 +309,6 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 					
 					@Override
 					public void onClick(View v) {
-						// TODO Auto-generated method stub
 						countTimer.cancel();
 					}
 				});
@@ -293,8 +331,7 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 
 			@Override
 			public void onClick(View v) {
-				totalCount++;
-				updateCounter();
+				increaseTotalCount();
 			}
 		});
 
@@ -306,7 +343,7 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 				if(totalCount >= 1){
 					totalCount--;
 				}
-				updateCounter();
+				updateCounters();
 			}
 		});
 	}
@@ -583,50 +620,8 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 
 		// Setting the currently selected object to the object that the user chose.
 		if(!key.equals("Comment")){
-			txt_currentObject.setText(value);
+			updateCurrentObjectTo(value);
 		}
-		
-//		if (key.equals("Bus")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Truck")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Car")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Motor Bike")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Pedestrian")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Bike")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Cane")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Dog")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Mobility Scooter")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Artificial Limb")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Crutches")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Walking Frame")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Back Brace - Visible")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Back Brace - Not Visible")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Leg Brace - Visible")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Leg Brace - Not Visible")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Wheel Chair - Assisted")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Wheel Chair - Manual")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Wheel Chair - Powered")) {
-//		Toast.makeText(this, value, Toast.LENGTH_SHORT).show();
-//	} else if (key.equals("Comment")){
-//		comments = value;
-//	}
 	}
 	
 	@Override
@@ -641,6 +636,195 @@ public class CountingScreen extends ActionBarActivity implements Communicator {
 
 	    return super.onKeyDown(keyCode, event);
 	}
+	
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == btn_direction_nw.getId()){
+			checkDirectionFromAndTo(btn_direction_nw);
+		}else if(v.getId() == btn_direction_n.getId()){
+			checkDirectionFromAndTo(btn_direction_n);
+		}else if(v.getId() == btn_direction_ne.getId()){
+			checkDirectionFromAndTo(btn_direction_ne);
+		}else if(v.getId() == btn_direction_w.getId()){
+			checkDirectionFromAndTo(btn_direction_w);
+		}else if(v.getId() == btn_direction_e.getId()){
+			checkDirectionFromAndTo(btn_direction_e);
+		}else if(v.getId() == btn_direction_sw.getId()){
+			checkDirectionFromAndTo(btn_direction_sw);
+		}else if(v.getId() == btn_direction_s.getId()){
+			checkDirectionFromAndTo(btn_direction_s);
+		}else if(v.getId() == btn_direction_se.getId()){
+			checkDirectionFromAndTo(btn_direction_se);
+		}
+		
+		Toast.makeText(this, "Direction From: " + directionFrom, Toast.LENGTH_SHORT).show();
+		Toast.makeText(this, "Direction To: " + directionTo, Toast.LENGTH_SHORT).show();
+	
+		updateAllCounts();
+	}
+	
+	private void checkDirectionFrom(Button button){
+		if(directionTo == null && directionFrom == null){
+			directionFrom = String.valueOf(button.getText());
+		}
+	}
+	
+	private void checkDirectionTo(Button button){
+		if(directionFrom != null){
+			if(!directionFrom.equals(String.valueOf(button.getText()))){
+				directionTo = String.valueOf(button.getText());
+			}
+		}
+	}
+	
+	private void checkDirectionFromAndTo(Button button){
+		if(directionFrom == null){
+			checkDirectionFrom(button);
+		}else{
+			checkDirectionTo(button);
+		}
+	}
+	
+	private void increaseTotalCount(){
+		totalCount++;
+		updateCounters();
+	}
+	
+	private void increaseObjectCount(){
+		Integer currentObject = null;
+		switch (currentlySelectedObject) {
+		case "Bus":
+			bus++;
+			currentObject = bus;
+//			Toast.makeText(this, currentlySelectedObject + ": " + bus, Toast.LENGTH_SHORT).show();
+			break;
+		case "Truck":
+			truck++;
+			currentObject = truck;
+//			Toast.makeText(this, currentlySelectedObject + ": " + truck, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Car":
+			car++;
+			currentObject = car;
+//			Toast.makeText(this, currentlySelectedObject + ": " + car, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Motor Bike":
+			motorBike++;
+			currentObject = motorBike;
+//			Toast.makeText(this, currentlySelectedObject + ": " + motorBike, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Pedestrian":
+			pedestrian++;
+			currentObject = pedestrian;
+//			Toast.makeText(this, currentlySelectedObject + ": " + pedestrian, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Bike":
+			bike++;
+			currentObject = bike;
+//			Toast.makeText(this, currentlySelectedObject + ": " + bike, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Cane":
+			cane++;
+			currentObject = cane;
+//			Toast.makeText(this, currentlySelectedObject + ": " + cane, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Dog":
+			dog++;
+			currentObject = dog;
+//			Toast.makeText(this, currentlySelectedObject + ": " + dog, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Mobility Scooter":
+			mobilityScooter++;
+			currentObject = mobilityScooter;
+//			Toast.makeText(this, currentlySelectedObject + ": " + mobilityScooter, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Artificial Limb":
+			artificialLimb++;
+			currentObject = artificialLimb;
+//			Toast.makeText(this, currentlySelectedObject + ": " + artificialLimb, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Crutches":
+			crutches++;
+			currentObject = crutches;
+//			Toast.makeText(this, currentlySelectedObject + ": " + crutches, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Walking Frame":
+			walkingFrame++;
+			currentObject = walkingFrame;
+//			Toast.makeText(this, currentlySelectedObject + ": " + walkingFrame, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Back Brace - Visible":
+			backBrace_visible++;
+			currentObject = backBrace_visible;
+//			Toast.makeText(this, currentlySelectedObject + ": " + backBrace_visible, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Back Brace - Not Visible":
+			backBrace_notVisible++;
+			currentObject = backBrace_notVisible;
+//			Toast.makeText(this, currentlySelectedObject + ": " + backBrace_notVisible, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Leg Brace - Visible":
+			legBrace_visible++;
+			currentObject = legBrace_visible;
+//			Toast.makeText(this, currentlySelectedObject + ": " + legBrace_visible, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Leg Brace - Not Visible":
+			legBrace_notVisible++;
+			currentObject = legBrace_notVisible;
+//			Toast.makeText(this, currentlySelectedObject + ": " + legBrace_notVisible, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Wheel Chair - Assisted":
+			wheelChair_assisted++;
+			currentObject = wheelChair_assisted;
+//			Toast.makeText(this, currentlySelectedObject + ": " + wheelChair_assisted, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Wheel Chair - Manual":
+			wheelChair_manual++;
+			currentObject = wheelChair_manual;
+//			Toast.makeText(this, currentlySelectedObject + ": " + wheelChair_manual, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Wheel Chair - Powered":
+			wheelChair_powered++;
+			currentObject = wheelChair_powered;
+//			Toast.makeText(this, currentlySelectedObject + ": " + wheelChair_powered, Toast.LENGTH_SHORT).show();
+			
+			break;
+		case "Comment":
+			
+			break;
+		default:
+			break;
+		}
+		
+		Toast.makeText(this, currentlySelectedObject + ": " + currentObject, Toast.LENGTH_SHORT).show();
+		updateCurrentlySelectedObject(currentObject);
+	}
+	
+	private void updateAllCounts(){
+		if(directionFrom != null && directionTo != null){
+			increaseObjectCount();
+			increaseTotalCount();
+			initialiseDirectionFromTo();
+		}
+	}
+	
 	private class CountDownTimer extends android.os.CountDownTimer{
 		
 		String ms;
